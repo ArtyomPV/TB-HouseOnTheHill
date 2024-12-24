e05b0a48c217c83e488ee7142eee56e309add59d
Artyom Prusov <itcoding76@gmail.com>
2024-12-24 05:47:54 +0200

    Initial commit

---
+++ src/main/java/ru/gb/TB_HouseOnTheHill/TbHouseOnTheHillApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.TB_HouseOnTheHill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbHouseOnTheHillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TbHouseOnTheHillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gb/TB_HouseOnTheHill/TbHouseOnTheHillApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gb.TB_HouseOnTheHill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbHouseOnTheHillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
